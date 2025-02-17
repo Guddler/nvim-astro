757c997d46e029b2bc0d9ec20016db103ac9a126
Martin White <Guddler@users.noreply.github.com>
2025-02-17 20:38:03 +0000

    Added json support

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,38 +2,38 @@
 
 ---@type LazySpec
 return {
-  -- use mason-lspconfig to configure LSP installations
-  {
-    "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        -- add more arguments for adding more language servers
-        "pyright",
-      },
-    },
-  },
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-  {
-    "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
-    opts = {
-      ensure_installed = {
-        "stylua",
-        -- add more arguments for adding more null-ls sources
-      },
-    },
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = {
-      ensure_installed = {
-        "python",
-        "debugpy",
-        -- add more arguments for adding more debuggers
-      },
-    },
-  },
+	-- use mason-lspconfig to configure LSP installations
+	{
+		"williamboman/mason-lspconfig.nvim",
+		-- overrides `require("mason-lspconfig").setup(...)`
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"pyright",
+			},
+		},
+	},
+	-- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+	{
+		"jay-babu/mason-null-ls.nvim",
+		-- overrides `require("mason-null-ls").setup(...)`
+		opts = {
+			ensure_installed = {
+				"stylua",
+				"jsonlint",
+				"fixjson",
+			},
+		},
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		-- overrides `require("mason-nvim-dap").setup(...)`
+		opts = {
+			ensure_installed = {
+				"python",
+				"debugpy",
+				-- add more arguments for adding more debuggers
+			},
+		},
+	},
 }
