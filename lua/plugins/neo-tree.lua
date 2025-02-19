525b206c48ab68056535334d79d67dc740170f57
Martin White <Guddler@users.noreply.github.com>
2025-02-19 11:30:17 +0000

    Created simple neo-tree config

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			default_component_configs = {
+				indent = {
+					with_markers = false,
+				},
+			},
+		},
+	},
+}
